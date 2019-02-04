76a3fc54bb80c698cea928641b1cd597d29df14c
Maksim Kostromin <daggerok@gmail.com>
2019-02-04 05:32:38 +0200

    Start writing... About page was initiated.

--- .vuepress/components/MyFooter.vue
+++ .vuepress/components/MyFooter.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="footer">
     <div>
-      MIT Licensed | Copyright © 2019 Максим Костромин
-      он же <a href="https://github.com/daggerok">daggerok</a>
+      Хрен его знает с правами, наверное они все защищены и все такое... Ho!
+      <br />
+      Использование материала, фотографий и прочей информации, размещенной на этом сайте
+      и / либо пренадлеженной мне, разрещено только после моего личглшл письменного подтверждения.
+      <br />
+      Уотакуот =)
+      <br />
+      <br />
+      MIT | Copyright © 2019
+      <br />
+      <br />
+      Максим Костромин он же <a href="https://github.com/daggerok">daggerok</a>
     </div>
   </div>
 </template>
